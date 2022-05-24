3cf655262000e2b9cb7ffba2a88a37657a3e23ff
Arkadiusz Klein <arek.klein@limebrains.com>
2022-05-24 19:06:23 +0200

    - remove log

--- app/main.dev.js
+++ app/main.dev.js
@@ -39,7 +39,6 @@ let historySentAlready = false;
 let macros = {};
 let macrosEnabled = true;
 
-const log = require('electron-log');
 const trayIcon =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACW0lEQVR42l2TyWvaQRTHR/ODQi8lqEhB7KEHD3pI0fZQGm8SevAs+Q+8eWpMKaVpLW0vYo6eSreLx9JCECGIkVIwKS49BNz3fd/3fkecjPYHw8y8ee8z3zfv/Qj94vE4qdVqZLFYiJbLJZnP5/cx27F/PhqNdrEns9mM0DObzbaa2+02ufkCgQB1ECOAzg+m02mt0Wj8hNNvBP8dDAZ3ACKTyURMg+kwmUwckEgkqFG8vv0MgR+YY6/XOwf0ZL3f8Xg81EYHB0SjUXq4ko/bL3HTAdZ3MagiG27/6HQ6BazvrW0km81yQCwWYwB6eFWv1w8Auuh2u48Ae9Xv9x248Rlsn9dKBKPRyAHJZHILUCgUniLQXyqVHg+Hw9cIdrRaLStsXxhArVZzQDqd3gLk83kKuCgWi5uAo/F4zAA7KpWKA1Kp1BYgl8utFEAJBZwgldNms3mEt/jKAEqlkgPgyAC0Cpd4oJUC2J+ghG8AcABg3QTI5fL/q8AVZDIZCvABtofeOEZZT9EXVqj5xgASiYQDIpHIJuAPOtMA5zPkbMPL/0CXvgTgGLYbBTKZjANCodBmH1xBtgENs9fpdGII/AWX2wC/RTk/MYBUKuUAl8u1MtJD5PkdN7/Dmra4gPdYdShSOkd/vGAAhULBAYIgkHK5LEbZ6HvsQ8UATofUGSWlgPeQX0Q3yoPBIEGVRDqdjgO8Xi/R6/UsDZrSIWpfhJJrjBR64BopPaxWq6RSqYjMZjPRaDQcQGvKfp5wOCyy2+3EYrHs+nw+g9vt3tdqtbeon9/vFzE/9oj/AOHffdTL+hwRAAAAAElFTkSuQmCC';
 
@@ -90,14 +89,6 @@ const clipboardWindowConfig = {
   },
 };
 
-function logger() {
-  log.transports.file.level = 'info';
-  log.transports.file.format = '{h}:{i}:{s}:{ms} {text}';
-  log.transports.file.maxSize = 3 * 1024 * 1024;
-}
-
-logger();
-
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
   sourceMapSupport.install();
@@ -113,8 +104,6 @@ if (isDebug) {
 
 const connectAutoUpdater = () => {
   autoUpdater.autoDownload = false;
-  autoUpdater.logger = log;
-  autoUpdater.on('error', e => log.error(`Update error ${e.message}`));
   autoUpdater.on('update-available', () => {
     updateAvailable = true;
     if (isMac) {
@@ -670,7 +659,6 @@ Your new history has ${clipboardHistory.length} entries.`,
                   });
                 }
               } catch (e) {
-                log.error(e);
                 dialog.showErrorBox('Error', 'Invalid backup file.');
               }
             }
