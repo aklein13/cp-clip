6639a85c891fc7f7908e1d541dc63c5b166d2dba
Arkadiusz Klein <arek.klein@gmail.com>
2022-05-28 20:24:36 +0200

    - fix setBounds fail if active screen is not found

--- app/main.dev.js
+++ app/main.dev.js
@@ -43,7 +43,6 @@ const trayIcon =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACW0lEQVR42l2TyWvaQRTHR/ODQi8lqEhB7KEHD3pI0fZQGm8SevAs+Q+8eWpMKaVpLW0vYo6eSreLx9JCECGIkVIwKS49BNz3fd/3fkecjPYHw8y8ee8z3zfv/Qj94vE4qdVqZLFYiJbLJZnP5/cx27F/PhqNdrEns9mM0DObzbaa2+02ufkCgQB1ECOAzg+m02mt0Wj8hNNvBP8dDAZ3ACKTyURMg+kwmUwckEgkqFG8vv0MgR+YY6/XOwf0ZL3f8Xg81EYHB0SjUXq4ko/bL3HTAdZ3MagiG27/6HQ6BazvrW0km81yQCwWYwB6eFWv1w8Auuh2u48Ae9Xv9x248Rlsn9dKBKPRyAHJZHILUCgUniLQXyqVHg+Hw9cIdrRaLStsXxhArVZzQDqd3gLk83kKuCgWi5uAo/F4zAA7KpWKA1Kp1BYgl8utFEAJBZwgldNms3mEt/jKAEqlkgPgyAC0Cpd4oJUC2J+ghG8AcABg3QTI5fL/q8AVZDIZCvABtofeOEZZT9EXVqj5xgASiYQDIpHIJuAPOtMA5zPkbMPL/0CXvgTgGLYbBTKZjANCodBmH1xBtgENs9fpdGII/AWX2wC/RTk/MYBUKuUAl8u1MtJD5PkdN7/Dmra4gPdYdShSOkd/vGAAhULBAYIgkHK5LEbZ6HvsQ8UATofUGSWlgPeQX0Q3yoPBIEGVRDqdjgO8Xi/R6/UsDZrSIWpfhJJrjBR64BopPaxWq6RSqYjMZjPRaDQcQGvKfp5wOCyy2+3EYrHs+nw+g9vt3tdqtbeon9/vFzE/9oj/AOHffdTL+hwRAAAAAElFTkSuQmCC';
 
 const server = new Server();
-// const isWindows = process.platform === 'win32';
 const isMac = process.platform === 'darwin';
 const isLinux = process.platform === 'linux';
 const isWindows = process.platform === 'win32';
@@ -185,36 +184,44 @@ const sendHistory = () => {
   }
 };
 
+
 const openWindow = () => {
   if (isMac) {
     app.dock.hide();
   }
-let nextWindowBounds = {};
-  if (!isLinux) {
-    const activeScreen = screen.getDisplayNearestPoint(
-      screen.getCursorScreenPoint()
-    );
-    const activeScreenBounds = activeScreen.bounds;
-    nextWindowBounds = {
-      x:
-        activeScreenBounds.x +
-        activeScreenBounds.width / 2 -
-        clipboardWindowConfig.width / 2,
-      y:
-        activeScreenBounds.y +
-        activeScreenBounds.height / 2 -
-        clipboardWindowConfig.height / 2 +
-        100,
-      width: clipboardWindowConfig.width,
-      height: clipboardWindowConfig.height,
-    };
-    clipboardWindow.setBounds(nextWindowBounds);
+  const activeScreen = screen.getDisplayNearestPoint(
+    screen.getCursorScreenPoint()
+  );
+  const activeScreenBounds = activeScreen.bounds;
+  let nextWindowBounds = {
+    x:
+      activeScreenBounds.x +
+      activeScreenBounds.width / 2 -
+      clipboardWindowConfig.width / 2,
+    y:
+      activeScreenBounds.y +
+      activeScreenBounds.height / 2 -
+      clipboardWindowConfig.height / 2 +
+      100,
+    width: clipboardWindowConfig.width,
+    height: clipboardWindowConfig.height,
+  };
+
+  // This happens when there is no active window for some reason
+  if (isNaN(nextWindowBounds.x) || isNaN(nextWindowBounds.y)) {
+    nextWindowBounds = undefined;
+  } else {
+    try {
+      clipboardWindow.setBounds(nextWindowBounds);
+    } catch (e) {}
   }
 
   clipboardWindow.show();
   // On Windows you can't set bounds before the window is shown
-  if (isWindows) {
-    clipboardWindow.setBounds(nextWindowBounds);
+  if (isWindows && nextWindowBounds) {
+    try {
+      clipboardWindow.setBounds(nextWindowBounds);
+    } catch (e) {}
   }
 
   clipboardWindow.setAlwaysOnTop(true, 'floating', 100);
